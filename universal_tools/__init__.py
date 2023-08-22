653e6103c20b8bb7ab3033d6ac58a501a70ebcf2
zhangzixu <justzzer@gmail.com>
2023-08-22 14:13:30 +0800

    [更新] 完善工具类项目

---
+++ universal_tools/__init__.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+"""
+@File     : __init__.py.py
+@Project  : mobile_control
+@Time     : 2023/8/22 10:24
+@Author   : Zhang ZiXu
+@Software : PyCharm
+@Desc     :  
+@Last Modify Time          @Version        @Author
+--------------------       --------        -----------
+2023/8/22 10:24            1.0             Zhang ZiXu
+"""
+# 钉钉测试告警群 token
+MONITOR_TOKEN = "3e39e86815aa18c6524a3d58fb8d494b82992b644153a3cb72545167c19d2d62"
+# 钉钉测试告警群签名
+MONITOR_SECERT = "SECe6012755c7c77d6f5780cfa69fdae9f0304722f1b4c8c69586c93954b80cb1f3"
